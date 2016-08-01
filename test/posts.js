3f9513195d4db63e9888018aa139416298c46b33
Eddie Jaoude <eddie@jaoudestudios.com>
2016-08-01 19:17:00 +0100

    Improved Post tests

--- test/posts.js
+++ test/posts.js
@@ -1,52 +1,70 @@
 var supertest = require('supertest');
 var should = require("should");
+var models  = require('../models');
 var server = supertest.agent("http://localhost:3000");
 
 describe('GET /posts', function() {
   it('respond with json', function(done) {
-    server
-      .get('/posts')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err,res){
-        res.status.should.equal(200);
-        res.body.length.should.be.above(1);
-      done();
+    var postData = {
+      title: 'Test Post title',
+      description: 'Test Post description'
+    };
+
+    models.post.create(postData).then(function(post) {
+      server
+        .get('/posts')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err,res){
+          res.status.should.equal(200);
+          res.body.length.should.be.above(1);
+        done();
+      });
     });
   });
 });
 
-describe('GET /posts/1', function() {
+describe('GET /posts/{id}', function() {
   it('respond with json', function(done) {
-    server
-      .get('/posts/1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err,res){
-        res.status.should.equal(200);
-        res.body.id.should.be.equal(1);
-      done();
+    var postData = {
+      title: 'Test Post title',
+      description: 'Test Post description'
+    };
+
+    models.post.create(postData).then(function(post) {
+      server
+        .get('/posts/' + post.id)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err,res){
+          res.status.should.equal(200);
+          res.body.id.should.be.equal(post.id);
+        done();
+      });
     });
   });
 });
 
 describe('POST /posts with no Tags', function() {
   it('respond with json', function(done) {
-    var postTitle = "New post title";
-    var postDescription = "New post description";
+    var postData = {
+      title: 'Test Post title',
+      description: 'Test Post description'
+    };
+
     server
       .post('/posts')
-      .send({title: postTitle, description: postDescription})
+      .send(postData)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(201)
       .end(function(err,res){
         res.status.should.equal(201);
         res.body.id.should.be.above(1);
-        res.body.title.should.be.equal(postTitle);
-        res.body.description.should.be.equal(postDescription);
+        res.body.title.should.be.equal(postData.title);
+        res.body.description.should.be.equal(postData.description);
       done();
     });
   });
@@ -54,22 +72,30 @@ describe('POST /posts with no Tags', function() {
 
 describe('POST /posts with Tags', function() {
   it('respond with json', function(done) {
-    var postTitle = "New post title with Tags";
-    var postDescription = "New post description with Tags";
-    var postTags = [{id: 1}, {id: 2}];
-    server
-      .post('/posts')
-      .send({title: postTitle, description: postDescription, tags: postTags})
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .end(function(err,res){
-        res.status.should.equal(201);
-        res.body.id.should.be.above(1);
-        res.body.title.should.be.equal(postTitle);
-        res.body.description.should.be.equal(postDescription);
-        res.body.tags.length.should.be.equal(2);
-      done();
+    var postData = {
+      title: 'Test Post title',
+      description: 'Test Post description'
+    };
+    var tagData = {
+      name: 'Test Tag for Post'
+    };
+
+    models.tag.create(tagData).then(function(tag) {
+      server
+        .post('/posts')
+        .send({title: postData.title, description: postData.description, tags: [{id: tag.id}] })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end(function(err,res){
+          res.status.should.equal(201);
+          res.body.id.should.be.above(1);
+          res.body.title.should.be.equal(postData.title);
+          res.body.description.should.be.equal(postData.description);
+          res.body.tags.length.should.be.equal(1);
+          res.body.tags[0].name.should.be.equal(tagData.name);
+        done();
+      });
     });
   });
 });
